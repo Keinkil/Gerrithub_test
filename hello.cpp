b074eab3552716c41f2af0061248bc706988e12a
Rikard Almgren <rikardal@axis.com>
2022-10-31 15:44:44 +0100

    test cpp binary2
    Change-Id: I219ff9445e2ad6e298b31cb9f9fe6231e90b073e

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,12 @@
-// Your First C++ Program
-
 #include <iostream>
 
 int main() {
+
     std::cout << "Hello World!";
+
+    // some new changes
+    std::cout << "Hello world!!"
+
     return 0;
+
 }
